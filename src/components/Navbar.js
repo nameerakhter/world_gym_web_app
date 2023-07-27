65e7611bd7d782907721dd883e128f8a94632863
Muhammad Nameer Akhter <akhtarnameer@gmail.com>
2023-07-27 20:15:09 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
